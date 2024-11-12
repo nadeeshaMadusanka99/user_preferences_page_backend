32e924289446cddee04cdd8733aa5d01ee15aec6
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2024-11-13 00:29:05 +0530

    Add account settings model and notification settings model

--- preferences/models.py
+++ preferences/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class AccountSettings(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+    bio = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class NotificationSettings(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email_notifications = models.BooleanField(default=True)
+    push_notifications = models.BooleanField(default=True)
+    notification_frequency = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user.username
