6cef6e6f7ffee22b66e4393d915d520ad2cf6909
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2024-11-13 15:54:35 +0530

    Add categories for the api doc endpoints

--- preferences/urls.py
+++ preferences/urls.py
@@ -3,10 +3,10 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r'account-settings', views.AccountSettingViewSet)
-router.register(r'notification-settings', views.NotificationSettingViewSet)
-router.register(r'theme-settings', views.ThemeSettingViewSet)
-router.register(r'privacy-settings', views.PrivacySettingViewSet)
+router.register(r'account-settings', views.AccountSettingViewSet, basename='account-settings')
+router.register(r'notification-settings', views.NotificationSettingViewSet, basename='notification-settings')
+router.register(r'theme-settings', views.ThemeSettingViewSet, basename='theme-settings')
+router.register(r'privacy-settings', views.PrivacySettingViewSet, basename='privacy-settings')
 
 urlpatterns = [
     path('', include(router.urls)),
--- preferences/views.py
+++ preferences/views.py
@@ -1,24 +1,59 @@
 from rest_framework import viewsets
+from drf_spectacular.utils import extend_schema_view, extend_schema
 from .models import AccountSetting, NotificationSetting, ThemeSetting, PrivacySetting
 from .serializers import AccountSettingSerializer, NotificationSettingSerializer, ThemeSettingSerializer, \
     PrivacySettingSerializer
 
 
+@extend_schema_view(
+    list=extend_schema(tags=["Account Settings"], description="List all account settings"),
+    create=extend_schema(tags=["Account Settings"], description="Create a new account setting"),
+    retrieve=extend_schema(tags=["Account Settings"], description="Retrieve an account setting"),
+    update=extend_schema(tags=["Account Settings"], description="Update an account setting"),
+    partial_update=extend_schema(tags=["Account Settings"], description="Partially update an account setting"),
+    destroy=extend_schema(tags=["Account Settings"], description="Delete an account setting"),
+)
 class AccountSettingViewSet(viewsets.ModelViewSet):
     queryset = AccountSetting.objects.all()
     serializer_class = AccountSettingSerializer
 
-
+@extend_schema_view(
+    list=extend_schema(tags=["Notification Settings"], description="List all notification settings"),
+    create=extend_schema(tags=["Notification Settings"], description="Create a new notification setting"),
+    retrieve=extend_schema(tags=["Notification Settings"], description="Retrieve a notification setting"),
+    update=extend_schema(tags=["Notification Settings"], description="Update a notification setting"),
+    partial_update=extend_schema(tags=["Notification Settings"], description="Partially update a notification setting"),
+    destroy=extend_schema(tags=["Notification Settings"], description="Delete a notification setting"),
+)
 class NotificationSettingViewSet(viewsets.ModelViewSet):
     queryset = NotificationSetting.objects.all()
     serializer_class = NotificationSettingSerializer
 
-
+@extend_schema_view(
+    list=extend_schema(tags=["Theme Settings"], description="List all theme settings"),
+    create=extend_schema(tags=["Theme Settings"], description="Create a new theme setting"),
+    retrieve=extend_schema(tags=["Theme Settings"], description="Retrieve a theme setting"),
+    update=extend_schema(tags=["Theme Settings"], description="Update a theme setting"),
+    partial_update=extend_schema(tags=["Theme Settings"], description="Partially update a theme setting"),
+    destroy=extend_schema(tags=["Theme Settings"], description="Delete a theme setting"),
+)
 class ThemeSettingViewSet(viewsets.ModelViewSet):
     queryset = ThemeSetting.objects.all()
     serializer_class = ThemeSettingSerializer
 
-
+@extend_schema_view(
+    list=extend_schema(tags=["Privacy Settings"], description="List all privacy settings"),
+    create=extend_schema(tags=["Privacy Settings"], description="Create a new privacy setting"),
+    retrieve=extend_schema(tags=["Privacy Settings"], description="Retrieve a privacy setting"),
+    update=extend_schema(tags=["Privacy Settings"], description="Update a privacy setting"),
+    partial_update=extend_schema(tags=["Privacy Settings"], description="Partially update a privacy setting"),
+    destroy=extend_schema(tags=["Privacy Settings"], description="Delete a privacy setting"),
+)
 class PrivacySettingViewSet(viewsets.ModelViewSet):
     queryset = PrivacySetting.objects.all()
     serializer_class = PrivacySettingSerializer
+
+    # Assigning a tag to categorize this endpoint under "Privacy Settings"
+    @extend_schema(tags=["Privacy Settings"])
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
