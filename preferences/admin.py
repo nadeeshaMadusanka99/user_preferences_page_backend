0559754f72c5847ad57a8e8289f95961ea651d83
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2024-11-13 12:53:39 +0530

    Configure admin for theme and privacy settings

--- preferences/admin.py
+++ preferences/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
-from .models import AccountSetting, NotificationSetting
+from .models import AccountSetting, NotificationSetting, ThemeSetting, PrivacySetting
+
 
 class AccountSettingAdmin(admin.ModelAdmin):
     list_display = ['username', 'email', 'bio']
 
+
 class NotificationSettingAdmin(admin.ModelAdmin):
     list_display = ['email_notifications', 'push_notifications', 'notification_frequency']
 
+
+class ThemeSettingAdmin(admin.ModelAdmin):
+    list_display = ['color', 'font', 'layout']
+
+
+class PrivacySettingAdmin(admin.ModelAdmin):
+    list_display = ['profile_visibility', 'data_sharing']
+
+
 admin.site.register(AccountSetting, AccountSettingAdmin)
 admin.site.register(NotificationSetting, NotificationSettingAdmin)
+admin.site.register(ThemeSetting, ThemeSettingAdmin)
+admin.site.register(PrivacySetting, PrivacySettingAdmin)
