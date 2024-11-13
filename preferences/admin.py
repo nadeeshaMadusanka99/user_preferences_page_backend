97d94c2065b4181c1eb029701725f21f128f7a34
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2024-11-13 12:40:14 +0530

    Add admin page views for the models

--- preferences/admin.py
+++ preferences/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import AccountSetting, NotificationSetting
 
-admin.site.register(AccountSetting)
-admin.site.register(NotificationSetting)
+class AccountSettingAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'bio']
+
+class NotificationSettingAdmin(admin.ModelAdmin):
+    list_display = ['email_notifications', 'push_notifications', 'notification_frequency']
+
+admin.site.register(AccountSetting, AccountSettingAdmin)
+admin.site.register(NotificationSetting, NotificationSettingAdmin)
