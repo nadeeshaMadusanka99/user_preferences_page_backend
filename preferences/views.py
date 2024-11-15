f8ac856553b05e594b8b59d81def1918b2a5a9de
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2024-11-15 15:39:28 +0530

    Add validations to the views

--- preferences/views.py
+++ preferences/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from drf_spectacular.utils import extend_schema_view, extend_schema
+from rest_framework.exceptions import PermissionDenied, NotFound
+
 from .models import AccountSetting, NotificationSetting, ThemeSetting, PrivacySetting
 from .serializers import AccountSettingSerializer, NotificationSettingSerializer, ThemeSettingSerializer, \
     PrivacySettingSerializer
@@ -15,17 +17,35 @@ class BasePreferenceSettingViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         user = self.request.user
 
+        if not user.is_authenticated:
+            raise PermissionDenied("You do not have permission to view this data.")
+
         if user.is_superuser:
-            return self.queryset.all()  # This will be set in the child viewsets
-        return self.queryset.filter(user=user)  # This will filter based on the user
+            queryset = self.queryset.all()
+        else:
+            queryset = self.queryset.filter(user=user)
+
+        if not queryset.exists():
+            raise NotFound(detail=f"No {self.queryset.model.__name__} data found for the user.")
+
+        return queryset
+
+    def perform_create(self, serializer):
+        try:
+            serializer.save(user=self.request.user)
+        except Exception as e:
+            raise ValidationError(f"Error creating object: {str(e)}")
 
     def destroy(self, request, *args, **kwargs):
         try:
             instance = self.get_object()
             instance.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
+        except self.queryset.model.DoesNotExist:
+            raise NotFound(detail=f"{self.queryset.model.__name__} not found.")
         except ValidationError as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
 
 @extend_schema_view(
     list=extend_schema(tags=["Account Settings"], description="List all account settings"),
@@ -38,6 +58,7 @@ class BasePreferenceSettingViewSet(viewsets.ModelViewSet):
 class AccountSettingViewSet(BasePreferenceSettingViewSet):
     serializer_class = AccountSettingSerializer
     queryset = AccountSetting.objects.all()
+
 
 @extend_schema_view(
     list=extend_schema(tags=["Notification Settings"], description="List all notification settings"),
@@ -63,6 +84,7 @@ class NotificationSettingViewSet(BasePreferenceSettingViewSet):
 class ThemeSettingViewSet(BasePreferenceSettingViewSet):
     serializer_class = ThemeSettingSerializer
     queryset = ThemeSetting.objects.all()
+
 
 @extend_schema_view(
     list=extend_schema(tags=["Privacy Settings"], description="List all privacy settings"),
